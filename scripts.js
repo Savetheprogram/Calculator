fd5f518bc8fd8c718733a573bc14e18bd98f14bb
Krzysztof Bożek <90691419+Savetheprogram@users.noreply.github.com>
2023-04-25 09:33:26 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,99 +1,144 @@
 
 let a = "";
-let b = "";
-let result = 0
+let b = 0;
+const buttonsAdd = []
 const displayScreenVariableA = document.querySelector('.variableA');
 const displayScreenVariableB = document.querySelector('.variableB');
 const displayOperator = document.querySelector('.operator')
 const displayResult = document.querySelector('.result')
 let mathematicalOperation = 0;
+let editVariable = 0
 
-const buttonAdd0 = document.querySelector('.button0');
-buttonAdd0.addEventListener('click', () => {
-    a += "0"
-    displayScreenVariableA.textContent = "a" + "=" + a;
-})
-const buttonAdd1 = document.querySelector('.button1');
-buttonAdd1.addEventListener('click', () => {
-    a += "1"
-    displayScreenVariableA.textContent = "a" + "=" + a;
-})
+for (let i =0; i<10; i++){
+    buttonsAdd[i] = document.querySelector('.button' + [i])
+    buttonsAdd[i].addEventListener('click', () => {
+        a += [i]
+        displayScreenVariableA.textContent = a
+    })}
 
-const buttonAdd2 = document.querySelector('.button2');
-buttonAdd2.addEventListener('click', () => {
-    a += "2"
-    displayScreenVariableA.textContent = "a" + "=" + a;
-})
-const buttonAdd3 = document.querySelector('.button3');
-buttonAdd3.addEventListener('click', () => {
-    a += "3"
-    displayScreenVariableA.textContent = "a" + "=" + a;
-})
 
-const buttonAdd4 = document.querySelector('.button4');
-buttonAdd4.addEventListener('click', () => {
-    a += "4"
-    displayScreenVariableA.textContent =  "a" + "=" + a;
+
+const negateButton = document.querySelector('.negateButton');
+negateButton.addEventListener('click', () => {
+    a = -a
+    displayScreenVariableA.textContent = a;
 })
-const buttonAdd5 = document.querySelector('.button5');
-buttonAdd5.addEventListener('click', () => {
-    a += "5"
-    displayScreenVariableA.textContent = "a" + "=" + a;
+const comaButton = document.querySelector('.comaButton')
+comaButton.addEventListener('click', () => {
+    a += "."
+    displayScreenVariableA.textContent = a;
 })
 
-const buttonAdd6 = document.querySelector('.button6');
-buttonAdd6.addEventListener('click', () => {
-    a += "6"
-    displayScreenVariableA.textContent = "a" + "=" + a;
-})
 
-const buttonAdd7 = document.querySelector('.button7');
-buttonAdd7.addEventListener('click', () => {
-    a += "7"
-    displayScreenVariableA.textContent = "a" + "=" + a;
-})
-
-const buttonAdd8 = document.querySelector('.button8');
-buttonAdd8.addEventListener('click', () => {
-    a += "8"
-    displayScreenVariableA.textContent = "a" + "=" + a;
-})
-
-const buttonAdd9 = document.querySelector('.button9');
-buttonAdd9.addEventListener('click', () => {
-    a += "9"
-    displayScreenVariableA.textContent = "a" + "=" + a;
-})
 function add () {
+    getResult()
+
+    editVariable == 0 ? b = a :  getResult() === a ? b = a : b = getResult()
     mathematicalOperation = 1;
-    displayResult.textContent = getResult()
-    b = a;
     a = '';
-    displayScreenVariableA.textContent = "a"+ '=' + a;
-    displayScreenVariableB.textContent = "b" + "=" +b;
+    displayScreenVariableA.textContent = a;
+    displayScreenVariableB.textContent = b;
     displayOperator.textContent = "+";
+    displayResult.textContent = "result " + "= " + getResult()
+    editVariable = 1
+
 }
+
+function subtract () {
+    getResult()
+
+    editVariable == 0 ? b = a :  getResult() === a ? b = a : b = getResult()
+    mathematicalOperation = 2;
+    a = '';
+    displayScreenVariableA.textContent = a;
+    displayScreenVariableB.textContent = b;
+    displayOperator.textContent = "-";
+    displayResult.textContent = "result " + "= " + getResult()
+    editVariable = 1
+}
+
+function multiply () {
+    getResult()
+
+    editVariable == 0 ? b = a :  getResult() === a ? b = a : b = getResult()
+    mathematicalOperation = 3;
+    a = '';
+    displayScreenVariableA.textContent = a;
+    displayScreenVariableB.textContent = b;
+    displayOperator.textContent = "*";
+    displayResult.textContent = "result " + "= " + getResult()
+    editVariable = 1
+}
+
+function divide () {
+    getResult()
+
+    editVariable == 0 ? b = a :  getResult() === a ? b = a : b = getResult()
+    mathematicalOperation = 4;
+    a = '';
+    displayScreenVariableA.textContent = a;
+    displayScreenVariableB.textContent = b;
+    displayOperator.textContent = "/";
+    displayResult.textContent = "result " + "= " + getResult();
+    editVariable = 1;
+}
+
 
 const addButton = document.querySelector('.buttonAdd')
 addButton.addEventListener('click', () => {
     add()
 })
+const subtractButton = document.querySelector('.buttonSubtract')
+subtractButton.addEventListener('click', () => {
+    subtract()
+})
+const multiplyButton = document.querySelector('.buttonMultiply')
+multiplyButton.addEventListener('click', () => {
+    multiply()
+})
+const divideButton = document.querySelector('.buttonDivide')
+divideButton.addEventListener('click', () => {
+    divide()
+})
+const resultButton = document.querySelector('.resultButton')
+resultButton.addEventListener('click', () => {
+    getResult(), displayResult.textContent = "result" + "=" + getResult()
+})
+const clearButton = document.querySelector('.clearButton')
+clearButton.addEventListener('click', () => {
+    clearDisplay()
+})
+const deleteDigitButton = document.querySelector('.backButton')
+deleteDigitButton.addEventListener('click', () => {
+    deleteDigit()
+})
 
 function getResult() {
     switch (mathematicalOperation) {
         case 1:
-            return +a + +b;
-            break;
+            return  +a + +b;
         case 2:
-            return +a - +b;
-            break;
+            return b - a;
         case 3:
-            return +a * +b;
-            break
+            return a * b;
         case 4:
-            return +a / +b;
-            break;
+            return b / a;
     }
+}
+function clearDisplay () {
+    a = ''
+    b = ''
+    editVariable = 0
+    displayScreenVariableB.textContent = ""
+    displayScreenVariableA.textContent = ""
+    displayResult.textContent = ""
+    displayOperator.textContent = ""
+}
+function deleteDigit () {
+    a = a.toString()
+    a = a.replace(a.charAt(a.length - 1), "")
+    displayScreenVariableA.textContent = a;
+
 }
 
 
